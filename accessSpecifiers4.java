3af15a42289aaf1b208bc846bd0f624f69ffc4a3
Harshithakr <97785447+Harshithakr@users.noreply.github.com>
2022-01-16 23:01:40 +0530

    Add files via upload

---
+++ accessSpecifiers4.java
@@ -0,0 +1,13 @@
+package ppp2;
+
+import ppp1.*;
+
+public class accessSpecifiers4 {
+
+	public static void main(String[] args) {
+		
+		pubaccess obj = new pubaccess(); 
+        obj.display();  
+		
+	}
+}
---
+++ pubaccess.java
@@ -0,0 +1,10 @@
+package ppp1;
+
+public class pubaccess {
+	public void display() 
+    { 
+        System.out.println("This is Public Access Specifiers"); 
+    } 
+}
+
+
