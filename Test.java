6dda338c5406421363b1ec01cbe9390817d499c4
Harshithakr <97785447+Harshithakr@users.noreply.github.com>
2022-01-19 11:54:49 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,49 +1,36 @@
-class Bicycle  
+package FileHandling;
+import java.io.IOException; 
+import java.nio.file.*; 
+  
+public class Test 
 { 
-    public int gear; 
-    public int speed; 
-    public Bicycle(int gear, int speed) 
+    public static void main(String[] args) 
     { 
-        this.gear = gear; 
-        this.speed = speed; 
-    } 
-    public void applyBrake(int decrement) 
-    { 
-        speed -= decrement; 
-    } 
-    public void speedUp(int increment) 
-    { 
-        speed += increment; 
-    }  
-    public String toString()  
-    { 
-        return("No of gears are " + gear + "\n" + "speed of bicycle is " + speed); 
-    }  
-} 
-class MountainBike extends Bicycle  
-{ 
-    public int seatHeight; 
-    public MountainBike(int gear,int speed,int startHeight) 
-    {  
-        super(gear, speed); 
-        seatHeight = startHeight; 
-    }  
-    public void setHeight(int newValue) 
-    { 
-        seatHeight = newValue; 
-    } 
-    @Override
-    public String toString() 
-    { 
-        return (super.toString()+ 
-                "\nseat height is "+seatHeight); 
+        try
+        { 
+            Files.deleteIfExists(Paths.get("C://temp//testFile2.txt")); 
+        } 
+        catch(NoSuchFileException e) 
+        { 
+            System.out.println("No such file/directory exists"); 
+        } 
+        catch(DirectoryNotEmptyException e) 
+        { 
+            System.out.println("Directory is not empty."); 
+        } 
+        catch(IOException e) 
+        { 
+            System.out.println("Invalid permissions."); 
+        } 
+          
+        System.out.println("Deletion successful."); 
     } 
 }
-public class Test  
-{ 
-    public static void main(String args[])  
-    { 
-        MountainBike mb = new MountainBike(3, 100, 25); 
-        System.out.println(mb.toString());
-    } 
-}
+
+/*public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}*/
