6b0f82bb6a6ef7e712e272d079e78ab0def00d16
Harshithakr <97785447+Harshithakr@users.noreply.github.com>
2022-03-07 15:38:04 +0530

    Delete project/Shoesproject directory

--- project/Shoesproject/src/main/java/com/project/shoes/jdbc/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.shoes.jdbc;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.shoes.entity.Product;
-public interface ProductRepository extends JpaRepository<Product, Long>{
-}
